17065f1153dba1a094a825d84bfe3532c45e9749
Matthias Saou <thias@marmotte.net>
2011-06-03 18:51:35 +0200

    Add new cosmetic::bash class for some simple yet useful cosmetic bash changes.

---
+++ modules/cosmetic/manifests/bash.pp
@@ -0,0 +1,24 @@
+# Class to modify globally some simple cosmetic aspects of the bash shell.
+#
+# Sample Usage :
+#    include cosmetic::bash
+#
+class cosmetic::bash (
+    $histtimeformat = '%Y%m%d %H:%M:%S ',
+    $ps1            = false,
+    $ensure         = 'present'
+) {
+
+    if $ensure == 'absent' {
+        file { "/etc/profile.d/cosmetic.sh": ensure => absent }
+    } else {
+        file { "/etc/profile.d/cosmetic.sh":
+            content => template("cosmetic/cosmetic.sh.erb"),
+            owner   => "root",
+            group   => "root",
+            mode    => 0755,
+        }
+    }
+
+}
+
