9504028844dfb84de981db7c675669a770d19fbb
Matthias Saou <thias@marmotte.net>
2011-06-08 14:17:33 +0200

    Update selinux module to 0.2.0 : Remove obsolete bool definition, since puppet provides a selbool type now and renamed the module definition to a more explicit audit2allow, which now supports messages from files outside the module.

---
+++ modules/selinux/manifests/audit2allow.pp
@@ -0,0 +1,44 @@
+# Local SELinux modules, created from avc denial messages to be allowed.
+#
+# You must copy the avc denial messages of what you want to allow to :
+# files/messages.<selinux_module_name>
+#
+# The module names loaded are automatically prefixed with "local" in order to
+# never conflict with modules from the currently loaded policy.
+# You can get a list of existing loaded modules with : semodule -l
+#
+# Sample Usage :
+#    selinux::audit2allow { "mydaemon": }
+#    selinux::audit2allow { "myotherdaemon":
+#        source => "puppet:///files/myotherdaemon/selinux-messages",
+#    }
+#
+define selinux::audit2allow (
+    $source = false
+) {
+
+    include selinux
+
+    # Parent directory and directory
+    file { "/etc/selinux/local": ensure => directory }
+    file { "/etc/selinux/local/${title}": ensure => directory }
+
+    # The deny messages we want to allow
+    file { "/etc/selinux/local/${title}/messages":
+        source => $source ? {
+            false   => "puppet:///modules/selinux/messages.${title}",
+            default => $source,
+        },
+    }
+
+    # Reload the changes automatically
+    exec { "audit2allow -M local${title} -i messages && semodule -i local${title}.pp":
+        require     => Package["audit2allow"],
+        path        => [ "/bin", "/usr/bin", "/sbin", "/usr/sbin" ],
+        cwd         => "/etc/selinux/local/${title}",
+        subscribe   => File["/etc/selinux/local/${title}/messages"],
+        refreshonly => true,
+    }
+
+}
+
