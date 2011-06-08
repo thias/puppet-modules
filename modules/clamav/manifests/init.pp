98a25016e90282a41b43c9f73b006272d0be2b5a
Matthias Saou <thias@marmotte.net>
2011-06-08 14:54:58 +0200

    New simple clamav module, only really here to get clamav::smtp to be used from postfix for now.

---
+++ modules/clamav/manifests/init.pp
@@ -0,0 +1,16 @@
+# Main ClamAV class.
+#
+# Since the Fedora/RHEL packages have changed a lot over time, but that only
+# the latest version(s) of ClamAV are actually supported, no efforts are made
+# to be compatible with older versions of the clamav-* packages.
+#
+# The current class and related definitions are meant for ClamAV 0.97.
+#
+class clamav {
+
+    package { "clamd": ensure => installed }
+
+    # TODO: Include freshclam related stuff here?
+
+}
+
