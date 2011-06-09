c6759765ee64458b221f47dfd5c30954be5cb1f4
Matthias Saou <thias@marmotte.net>
2011-06-09 20:52:16 +0200

    New mailman module.

---
+++ modules/mailman/Modulefile
@@ -0,0 +1,4 @@
+name 'mailman'
+version '0.1.0'
+dependency 'apache-httpd', '>= 0.1.0'
+dependency 'postfix', '>= 0.1.0'
---
+++ modules/mailman/tests/init.pp
@@ -0,0 +1,14 @@
+class { "mailman":
+    default_url_host => "lists.example.org",
+    default_email_host => "example.org",
+    default_url_pattern => "https://%s/mailman/",
+    mailman_site_list => "mailman-list",
+    mm_cfg_settings => {
+        'ALLOW_SITE_ADMIN_COOKIES' => "Yes",
+        'PUBLIC_ARCHIVE_URL' => "'https://%(hostname)s/pipermail/%(listname)s'",
+        'MTA' => "'Postfix'",
+        'POSTFIX_STYLE_VIRTUAL_DOMAINS' => "'False'",
+        'DEFAULT_SUBJECT_PREFIX' => "''",
+        'DEFAULT_REPLY_GOES_TO_LIST' => "1",
+    },
+}
