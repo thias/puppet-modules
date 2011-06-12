8caed1d8abb16742f3c69318352fd6330840a902
Matthias Saou <thias@marmotte.net>
2011-06-13 00:13:05 +0200

    Minor style and 0.27+ compatibility cleanups in the selinux module.

--- modules/selinux/manifests/init.pp
+++ modules/selinux/manifests/init.pp
@@ -3,24 +3,24 @@
 #
 class selinux {
 
-    if $selinux and $selinux_enforced {
+    if $::selinux and $::selinux_enforced {
         # The audit2allow tool was split out in -python at some point
-        $audit2allow = "${operatingsystem}-${operatingsystemrelease}" ? {
-            'Fedora-10' => "policycoreutils",
-             default    => "policycoreutils-python",
+        $audit2allow = "${::operatingsystem}-${::operatingsystemrelease}" ? {
+            'Fedora-10' => 'policycoreutils',
+             default    => 'policycoreutils-python',
         }
-        package { "libselinux-ruby": ensure => installed }
+        package { 'libselinux-ruby': ensure => installed }
         package { $audit2allow:
-            alias  => "audit2allow",
+            alias  => 'audit2allow',
             ensure => installed,
         }
-        service { "restorecond":
+        service { 'restorecond':
             enable    => true,
             ensure    => running,
             hasstatus => true,
         }
         # The parent directory used from selinux::audit2allow
-        @@file { "/etc/selinux/local": ensure => directory }
+        @@file { '/etc/selinux/local': ensure => directory }
     }
 
 }
