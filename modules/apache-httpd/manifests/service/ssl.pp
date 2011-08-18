e40e9844fbef11b44dc047fee77eb7093a1c64e9
Matthias Saou <matthias@saou.eu>
2011-08-18 17:39:04 +0200

    Improve comments for puppetdoc output.

--- modules/apache-httpd/manifests/service/ssl.pp
+++ modules/apache-httpd/manifests/service/ssl.pp
@@ -1,3 +1,7 @@
+# Apache httpd web server service ssl class, inherited from the base class.
+#
+# We add the mod_ssl package with a stipped-down configuration.
+#
 class apache-httpd::service::ssl inherits apache-httpd::service::base {
 
     # Main package and service changes
@@ -10,6 +14,7 @@ class apache-httpd::service::ssl inherits apache-httpd::service::base {
     # We disable everything in the file except loading the module
     # To listen on 443, the directive is required in an apache-httpd::file
     apache-httpd::file { 'ssl.conf':
+        require => Package['mod_ssl'],
         content => template('apache-httpd/conf.d/ssl.conf'),
     }
 
