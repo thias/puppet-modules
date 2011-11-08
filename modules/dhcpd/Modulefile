69021c4d150614ee0d33d6a632e9eb81e7dc4480
Matthias Saou <matthias@saou.eu>
2011-11-08 16:19:10 +0100

    Initial dhcpd module

---
+++ modules/dhcpd/Modulefile
@@ -0,0 +1,2 @@
+name 'dhcpd'
+version '0.1.0'
---
+++ modules/dhcpd/manifests/init.pp
@@ -0,0 +1,38 @@
+# DHCP module.
+#
+# Sample Usage :
+#     class { 'dhcpd':
+#         configsource => 'puppet:///files/dhcpd.conf-foo',
+#         # Restrict listening to a single interface
+#         dhcpdargs    => 'br1',
+#     }
+#
+class dhcpd (
+    $configsource,
+    $dhcpdargs = '',
+    $ensure = 'running',
+    $enable = true
+) {
+
+    package { 'dhcp': ensure => installed }
+
+    service { 'dhcpd':
+        ensure    => $ensure,
+        enable    => $enable,
+        hasstatus => true,
+        require   => Package['dhcp'],
+    }
+
+    file { '/etc/sysconfig/dhcpd':
+        content => template('dhcpd/dhcpd.sysconfig.erb'),
+        notify  => Service['dhcpd'],
+    }
+
+    file { '/etc/dhcp/dhcpd.conf':
+        source  => $configsource,
+        require => Package['dhcp'],
+        notify  => Service['dhcpd'],
+    }
+
+}
+
