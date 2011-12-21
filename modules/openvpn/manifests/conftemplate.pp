4f2289422766ccf15c488fba0aa051db22bd3c11
Matthias Saou <matthias@saou.eu>
2011-12-21 11:27:13 +0100

    New OpenVPN module

---
+++ modules/openvpn/manifests/conftemplate.pp
@@ -0,0 +1,39 @@
+define openvpn::conftemplate (
+    $dir = '/etc/openvpn',
+    # Template options
+    $dev,
+    $remote,
+    $float = false,
+    $ipaddress_local = $ipaddress,
+    $ipaddress_remote,
+    $routes = [],
+    $secret,
+    $port = '1194'
+) {
+
+    include openvpn
+
+    file { "${dir}/${title}.conf":
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0640',
+        content => template('openvpn/default.conf.erb'),
+        # The secret file referenced, which already requires the parent dir
+        require => File["${dir}/${secret}"],
+    }
+    # start
+    exec { "openvpn-start-${title}":
+        command => "/usr/sbin/openvpn --daemon --writepid /var/run/openvpn/${title}.pid --config ${title}.conf --cd ${dir}",
+        cwd     => $dir,
+        require => File["${dir}/${title}.conf"],
+        creates => "/var/run/openvpn/${title}.pid",
+    }
+    # FIXME : stop/restart
+    exec { "openvpn-stop-${title}":
+        command => "kill `cat /var/run/openvpn/${title}.pid`",
+        path    => [ '/bin', '/usr/bin' ],
+        refreshonly => true, 
+    }
+
+}
+
