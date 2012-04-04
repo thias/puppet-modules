d0e6174f097d3eac991c8020522a491b88a753c7
Matthias Saou <matthias@saou.eu>
2012-04-04 11:18:51 +0200

    Major clean up of the cgit module

--- modules/cgit/manifests/init.pp
+++ modules/cgit/manifests/init.pp
@@ -1,73 +1,71 @@
-# Cgit definition.
+# Class: cgit
 #
 # Sample Usage :
-#     apache-httpd { 'worker':
-#         modules   => [ 'mime', 'setenvif', 'alias', 'proxy', 'cgi' ],
-#         keepalive => 'On',
-#         user      => 'git',
-#         group     => 'git',
-#     }
-#     class { 'cgit':
-#         httpd_path      => '/git',
-#         httpd_path_data => '/git-data',
-#         clone_prefix    => 'git://git.marmotte.net',
-#         css             => '/git-data/cgit.css',
-#         logo            => '/git-data/pink-spaceinvader.png',
-#         logo_link       => 'http://thias.marmotte.net/',
-#         favicon         => 'http://thias.marmotte.net/favicon.ico',
-#         snapshots       => 'tar.gz',
-#         max_stats       => false,
-#         root_title      => 'Git repositories on marmotte.net',
-#         root_desc       => 'commit, push... et alors, la marmotte...',
-#         repos           => [
-#             'puppet-modules' => {
-#                 'url'   => 'puppet-modules',
-#                 'path'  => '/srv/git/repositories/puppet-modules.git',
-#                 'desc'  => 'Custom Puppet Modules',
-#                 'owner' => 'Matthias Saou',
-#             }
-#         ],
-#     }
-#     # Default is apache:root
-#     file { '/var/cache/cgit':
-#         ensure => directory,
-#         owner  => 'git',
-#     }
+#  apache_httpd { 'worker':
+#      modules   => [ 'mime', 'setenvif', 'alias', 'proxy', 'cgi' ],
+#      keepalive => 'On',
+#      user      => 'git',
+#      group     => 'git',
+#  }
+#  class { 'cgit':
+#      httpd_path      => '/git',
+#      httpd_path_data => '/git-data',
+#      clone_prefix    => 'git://git.marmotte.net',
+#      css             => '/git-data/cgit.css',
+#      logo            => '/git-data/pink-spaceinvader.png',
+#      logo_link       => 'http://thias.marmotte.net/',
+#      favicon         => 'http://thias.marmotte.net/favicon.ico',
+#      snapshots       => 'tar.gz',
+#      max_stats       => false,
+#      root_title      => 'Git repositories on marmotte.net',
+#      root_desc       => 'commit, push... et alors, la marmotte...',
+#      repos           => {
+#          'puppet-modules' => {
+#              'path'  => '/srv/git/repositories/puppet-modules.git',
+#              'desc'  => 'Custom Puppet Modules',
+#              'owner' => 'Matthias Saou',
+#          },
+#      },
+#  }
+#  # Default is apache:root
+#  file { '/var/cache/cgit':
+#      ensure => directory,
+#      owner  => 'git',
+#  }
 #
 class cgit (
-    $httpd_path = '/cgit',
-    $httpd_path_data = '/cgit-data',
+    $httpd_conf             = true,
+    $httpd_path             = '/cgit',
+    $httpd_path_data        = '/cgit-data',
     # allow using an entirely custom cgitrc file
-    $cgitrc_source = undef,
-    $cgitrc_content = undef,
+    $cgitrc_source          = undef,
+    $cgitrc_content         = undef,
     # cgitrc options when using the provided template
-    $cache_size = '1000',
-    $clone_prefix = false,
-    $css = '/cgit-data/cgit.css',
-    $enable_index_links = '1',
-    $enable_commit_graph = '1',
-    $enable_log_filecount = '1',
-    $enable_log_linecount = '1',
-    $favicon = false,
-    $logo = '/cgit-data/cgit.png',
-    $logo_link = false,
-    $max_stats = 'quarter',
-    $root_title = false,
-    $root_desc = false,
-    $root_readme = false,
-    $snapshots = false,
-    $mimetypes = [],
-    $source_filter = false,
-    $repos = [],
-    $scan_path = false,
-    $remove_suffix = false
+    $cache_size             = '1000',
+    $clone_prefix           = false,
+    $css                    = '/cgit-data/cgit.css',
+    $enable_index_links     = '1',
+    $enable_commit_graph    = '1',
+    $enable_log_filecount   = '1',
+    $enable_log_linecount   = '1',
+    $favicon                = false,
+    $logo                   = '/cgit-data/cgit.png',
+    $logo_link              = false,
+    $max_stats              = 'quarter',
+    $root_title             = false,
+    $root_desc              = false,
+    $root_readme            = false,
+    $snapshots              = false,
+    $mimetypes              = [],
+    $source_filter          = false,
+    $repos                  = {},
+    $scan_path              = false,
+    $remove_suffix          = false
 ) {
 
     # Main package
     package { 'cgit':
         ensure  => installed,
-        # You want to also be using the 'apache-httpd' module
-        require => Package['httpd'],
     }
 
     # Main configuration file
@@ -83,9 +81,12 @@ class cgit (
         source  => $cgitrc_source,
     }
 
-    # Apache httpd configuration snippet
-    file { '/etc/httpd/conf.d/cgit.conf':
-        content => template('cgit/httpd-cgit.conf.erb'),
+    if $httpd_conf {
+        # Apache httpd configuration snippet
+        file { '/etc/httpd/conf.d/cgit.conf':
+            content => template('cgit/httpd-cgit.conf.erb'),
+            notify  => Service['httpd'],
+        }
     }
 
 }
