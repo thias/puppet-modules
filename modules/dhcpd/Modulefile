c3128239ed1e3acd0ddfc36fcfff450e7b538b50
Matthias Saou <matthias@saou.eu>
2012-04-08 13:42:37 +0200

    Clean up the dhcpd module to match current puppetlabs guidelines

--- modules/dhcpd/Modulefile
+++ modules/dhcpd/Modulefile
@@ -1,2 +1,9 @@
 name 'dhcpd'
-version '0.1.1'
+version '0.2.0'
+source 'git://github.com/thias/puppet-modules/modules/dhcpd'
+author 'Matthias Saou'
+license 'Apache 2.0'
+summary 'DHCP Daemon'
+description "Install, enable and configure a Dynamic Host Control Protocol
+    (DHCP) server."
+project_page 'http://glee.thias.es/puppet'
--- modules/dhcpd/manifests/init.pp
+++ modules/dhcpd/manifests/init.pp
@@ -1,10 +1,24 @@
 # Class: dhcpd
 #
+# Installs and enables a dhcpd server.
+#
+# Parameters:
+#  $configsource:
+#    Puppet location of the configuration file to use. Mandatory.
+#  $dhcpdargs:
+#    Command-line arguments to be added to dhcpd. Default: empty
+#  $ensure:
+#    Ensure parameter for the service. Default: undefined
+#  $enable:
+#    Enable parameter for the service. Default: true
+#
 # Sample Usage :
 #  class { 'dhcpd':
 #      configsource => 'puppet:///files/dhcpd.conf-foo',
 #      # Restrict listening to a single interface
 #      dhcpdargs    => 'br1',
+#      # Default is to enable but allow to be stopped (for active/passive)
+#      ensure       => 'running',
 #  }
 #
 class dhcpd (
