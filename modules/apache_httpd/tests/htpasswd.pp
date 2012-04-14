18beae0f401766b27e8266b752d00b2ebfb5bf4b
Jared Curtis <jcurtis@ncircle.com>
2012-04-13 23:32:39 -0700

    left off a ;

--- modules/apache_httpd/tests/htpasswd.pp
+++ modules/apache_httpd/tests/htpasswd.pp
@@ -3,7 +3,7 @@ Apache_httpd::Htpasswd {
 }
 apache_httpd::htpasswd { 
   'user0':
-    password => '$apr1$gW9nYXAe$Nc4v5xoPcFhjdVZHFaUNT0'
+    password => '$apr1$gW9nYXAe$Nc4v5xoPcFhjdVZHFaUNT0';
   'user1':
     password => '$apr1$RNvZVTO9$CyU8OfyVZMbdmx4534ngz0',
     ensure   => absent;
