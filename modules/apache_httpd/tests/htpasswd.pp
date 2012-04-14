7592408ace594049219c6a1768bc33d510cecbe5
Jared Curtis <jcurtis@ncircle.com>
2012-04-13 23:27:52 -0700

    test class for htpasswd

---
+++ modules/apache_httpd/tests/htpasswd.pp
@@ -0,0 +1,13 @@
+Apache_httpd::Htpasswd { 
+    file => '/etc/httpd/htpasswd'
+}
+apache_httpd::htpasswd { 
+  'user0':
+    password => '$apr1$gW9nYXAe$Nc4v5xoPcFhjdVZHFaUNT0'
+  'user1':
+    password => '$apr1$RNvZVTO9$CyU8OfyVZMbdmx4534ngz0',
+    ensure   => absent;
+  'user2':
+    password => '$apr1$uhx4s3XL$muKjLJNnEIISBrhoyFp241';
+}
+
