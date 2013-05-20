43a5c7dd13921a8f5c53b91910c9ab971ab6b978
Matthias Saou <matthias@saou.eu>
2013-05-20 19:13:46 +0200

    Remove ipset, moved to its own repo

--- modules/ipset/tests/init.pp
+++
@@ -1,7 +0,0 @@
-file { '/tmp/test-iplist.txt':
-    source => "puppet:///modules/${module_name}/test-iplist.txt",
-}
-ipset { 'foo':
-    from_file => '/tmp/test-iplist.txt',
-}
-ipset { 'bar': ensure => absent }
--- modules/ipset/tests/params.pp
+++
@@ -1,1 +0,0 @@
-include ipset::params
