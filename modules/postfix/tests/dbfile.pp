0087f4ff5860d25fd5860d7f136bd18d7550a8eb
Matthias Saou <matthias@saou.eu>
2013-05-31 12:48:48 +0200

    Replace postfix module content with git submodule

--- modules/postfix/tests/dbfile.pp
+++
@@ -1,6 +0,0 @@
-# Required for the service to be notified
-include postfix::server
-postfix::dbfile { 'virtual':
-    content => 'puppet: root, you, me, foo@example.com',
-}
-
--- modules/postfix/tests/file.pp
+++
@@ -1,6 +0,0 @@
-# Required for the service to be notified
-include postfix::server
-postfix::file { 'example':
-    content => "# Nothing to see here...\n",
-}
-
