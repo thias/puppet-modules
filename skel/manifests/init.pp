b5d8da6370e1967974d486e953c30320c4867769
Matthias Saou <matthias@saou.eu>
2014-12-16 15:56:15 +0100

    Update skeleton module, replace Modulefile with metadata.json

--- skel/manifests/init.pp
+++ skel/manifests/init.pp
@@ -1,17 +1,3 @@
-# Define: 
-# Class: 
-#
-# Description text.
-#
-# Parameters:
-#  $ensure:
-#    Whether XYZ should be 'present' or 'absent'. Defaults to 'present'.
-#  $other:
-#    Optional other. Default: none
-#
-# Sample Usage :
-#  include skel
-#
 class skel {
 }
 
